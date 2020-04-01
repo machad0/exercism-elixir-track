c3d27cf183277061354036fa52822ce9153d7248
Lucas Machado Figueira <lucas.figueira@gmail.com>
2020-03-31 22:28:24 -0300

    refactor: 💡 instructor proposed refactor

--- beer-song/beer_song.ex
+++ beer-song/beer_song.ex
@@ -3,24 +3,26 @@ defmodule BeerSong do
   Get a single verse of the beer song
   """
   @spec verse(integer) :: String.t()
+  def verse(0) do
+    """
+    No more bottles of beer on the wall, no more bottles of beer.
+    Go to the store and buy some more, 99 bottles of beer on the wall.
+    """
+  end
+
   def verse(number) do
-    """
-    #{first_verse(number)}
-    #{second_verse(number)}
-    """
+    sing(number)
   end
 
-  defp first_verse(0), do: "No more bottles of beer on the wall, no more bottles of beer."
-  defp first_verse(number) do
-    number_bottles = beer(number) 
-    ~s(#{number_bottles} of beer on the wall, #{number_bottles} of beer.)
+  defp sing(0) do
   end
-
-  defp second_verse(0), do: "Go to the store and buy some more, 99 bottles of beer on the wall."
-  defp second_verse(number) do
-    number_bottles = beer(number - 1)
-    ~s(Take #{which?(number)} down and pass it around, #{number_bottles} of beer on the wall.)
-  end
+  defp sing(number) do
+    number_bottles = beer(number)
+    """
+    #{number_bottles} of beer on the wall, #{number_bottles} of beer.
+    Take #{which?(number)} down and pass it around, #{beer(number - 1)} of beer on the wall.
+     """
+     end
 
   defp which?(1), do: "it"
   defp which?(_number), do: "one"
@@ -33,7 +35,6 @@ defmodule BeerSong do
   Get the entire beer song for a given range of numbers of bottles.
   """
   def lyrics(), do: lyrics(99..0)
-
   @spec lyrics(Range.t()) :: String.t()
   def lyrics(range) do
     verses = for bottle <- range, do: verse(bottle)
